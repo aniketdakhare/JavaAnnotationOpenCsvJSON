094b70ad302a4a813f5f1ca19f350c5d625cbd79
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-24 03:45:51 +0530

    REFACTORED POJO class

--- src/main/java/com/bridgelabz/opencsv/model/CSVUser.java
+++ src/main/java/com/bridgelabz/opencsv/model/CSVUser.java
@@ -16,6 +16,17 @@ public class CSVUser
     @CsvBindByName
     private String country;
 
+    public CSVUser() {
+    }
+
+    public CSVUser(String name, String email, String phoneNo, String country)
+    {
+        this.setName(name);
+        this.setEmail(email);
+        this.setPhoneNo(phoneNo);
+        this.setCountry(country);
+    }
+
     @Override
     public String toString() {
         return "CSVUser{" +
@@ -24,5 +35,37 @@ public class CSVUser
                 ", phoneNo='" + phoneNo + '\'' +
                 ", country='" + country + '\'' +
                 '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
